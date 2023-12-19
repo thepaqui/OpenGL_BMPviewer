c0fe0036dac2f791eb88777f163dedf614a81ddb
thepaqui <theopaquier2@gmail.com>
2023-12-19 19:32:38 +0100

    Continued bmp parsing (and finally understood it)

--- include/bmp.hpp
+++ include/bmp.hpp
@@ -6,7 +6,7 @@
 /*   By: thepaqui <thepaqui@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:02:30 by thepaqui          #+#    #+#             */
-/*   Updated: 2023/12/18 18:29:55 by thepaqui         ###   ########.fr       */
+/*   Updated: 2023/12/19 19:16:19 by thepaqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,60 @@
 # include <iostream>
 # include <fstream>
 # include <filesystem>
+# include <stdexcept>
 
+// 8 bits / unsigned
 typedef	__UINT8_TYPE__	BYTE;
+// 16 bits / 2 bytes / unsigned
 typedef	__UINT16_TYPE__	WORD;
+// 32 bits / 4 bytes / unsigned
 typedef	__UINT32_TYPE__	DWORD;
+// 32 bits / 4 bytes / signed
+typedef	__INT32_TYPE__	SDWORD;
 
 // This enforces tight packing of the structures
 #pragma pack(push, 1)
 
-struct BMPFileHeader
+// Size = 14 bytes
+typedef struct	BMPFileHeader
 {
-	WORD	fileType;	// Always BM (0x4D42) for BMP files
-	DWORD	fileSize;	// In bytes
-	WORD	reserved1;	// Reserved values, can be ignored here
-	WORD	reserved2;	// Reserved values, can be ignored here
-	DWORD	dataOffset;	// Offset at which the pixel data starts in file
-};
+	WORD	fileType{0};	// Always BM (0x4D42) for BMP files
+	DWORD	fileSize{0};	// In bytes
+	WORD	reserved1{0};	// Reserved values, can be ignored here
+	WORD	reserved2{0};	// Reserved values, can be ignored here
+	DWORD	dataOffset{0};	// Offset at which the pixel data starts in file
+}				t_FileHeader;
+
+// Size = 40 bytes
+// This header has multiple versions, I only take BITMAPINFOHEADER
+// Supported by at least Windows NT, 3.1x
+typedef struct	BMPInfoHeader
+{
+	DWORD	size{0};			// Size of this header (in bytes) (includes color header and bit mask info size if present)
+	SDWORD	width{0};			// width of BMP image data in pixels
+	SDWORD	height{0};			// height of BMP image data in pixels (if positive, data starts with bottom line of the image)
+	WORD	planes{1};			// Always 1
+	WORD	bpp{0};				// Bits per pixel
+	DWORD	compression{0};		// 0 (uncompresed BGR) or 3 (uncompressed BGRA). Anything else is compressed or unsupported.
+	DWORD	rawDataSize{0};		// Size of image's raw data in bytes (can be 0 if uncompressed)
+	SDWORD	xPixelsPerMeter{0};
+	SDWORD	yPixelsPerMeter{0};
+	DWORD	colorsUsed{0};		// No. color indexes in the color table. Use 0 for the max number of colors allowed by bit_count
+	DWORD	colorsImportant{0};	// No. of colors used for displaying the bitmap. If 0 all colors are required
+}				t_InfoHeader;
+
+// Size = 84 bytes
+// This header is only present for transparent images (BGRA32bit)
+// BGR24 does not use this or bit ma
+typedef struct	BMPColorHeader
+{
+	DWORD	redMask{0x00ff0000};		// Bit mask for the red channel
+	DWORD	greenMask{0x0000ff00};		// Bit mask for the green channel
+	DWORD	blueMask{0x000000ff};		// Bit mask for the blue channel
+	DWORD	alphaMask{0xff000000};		// Bit mask for the alpha channel
+	DWORD	colorSpaceType{0x73524742};	// Default "sBGR" (0x73524742)
+	DWORD	unused[16]{0};				// Unused data for sBGR color space
+}				t_ColorHeader;
 
 #pragma pack(pop)
 
--- src/bmp.cpp
+++ src/bmp.cpp
@@ -6,7 +6,7 @@
 /*   By: thepaqui <thepaqui@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:08:15 by thepaqui          #+#    #+#             */
-/*   Updated: 2023/12/18 18:39:30 by thepaqui         ###   ########.fr       */
+/*   Updated: 2023/12/19 18:48:57 by thepaqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,66 @@ inline bool	isFileEmpty(std::ifstream &file)
 	return (static_cast<int>(file.peek()) == std::ifstream::traits_type::eof());
 }
 
+// Opens in binary mode
 static std::ifstream	openIFile(const std::filesystem::path &filePath)
 {
 	std::ifstream	ifs;
-	ifs.open(filePath);
+	ifs.open(filePath, std::ifstream::ios_base::binary);
 	if (!ifs.is_open() || ifs.fail())
 	{
 		std::cout << "ERROR WHILE OPENING " << filePath << std::endl;
-		throw std::exception();
+		throw std::runtime_error("Could not open " + filePath.string());
 	}
 	if (isFileEmpty(ifs))
 	{
 		std::cout << filePath << " IS EMPTY" << std::endl;
 		ifs.close();
-		throw std::exception();
+		throw std::runtime_error(filePath.string() + " is empty");
 	}
 	return ifs;
+}
+
+// Reads n bytes from ifs into dst
+static void	readNBytes(std::ifstream &ifs, char *dst, size_t n)
+{
+	ifs.read(dst, n);
+}
+
+static void	printFileHeader(t_FileHeader &fh)
+{
+	std::cout << std::hex
+		<< "fileType =	0x"		<< fh.fileType		<< "\n"
+		<< "fileSize =	0x"		<< fh.fileSize		<< "\n"
+		<< "reserved1 =	0x"		<< fh.reserved1		<< "\n"
+		<< "reserved2 =	0x"		<< fh.reserved2		<< "\n"
+		<< "dataOffset =	0x"	<< fh.dataOffset	<< "\n"
+		<< std::dec << std::endl;
+}
+
+static void	printInfoHeader(t_InfoHeader &ih)
+{
+	std::cout << std::hex
+		<< "size =			0x"		<< ih.size				<< "\n"
+		<< "width =			0x"		<< ih.width				<< "\n"
+		<< "height =		0x"		<< ih.height			<< "\n"
+		<< "planes =		0x"		<< ih.planes			<< "\n"
+		<< "bpp =			0x"		<< ih.bpp				<< "\n"
+		<< "compression =		0x"	<< ih.compression		<< "\n"
+		<< "rawDataSize =		0x"	<< ih.rawDataSize		<< "\n"
+		<< "xPixelsPerMeter =	0x"	<< ih.xPixelsPerMeter	<< "\n"
+		<< "yPixelsPerMeter =	0x"	<< ih.yPixelsPerMeter	<< "\n"
+		<< "colorsUsed =		0x"	<< ih.colorsUsed		<< "\n"
+		<< "colorsImportant =	0x"	<< ih.colorsImportant	<< "\n"
+		<< std::dec << std::endl;
+}
+
+static void	checkInfoHeader(const std::filesystem::path &filePath, t_InfoHeader &ih)
+{
+	if (ih.bpp != 24 && ih.bpp != 32)
+	{
+		std::cout << filePath << ": COLOR ENCODING UNSUPPORTED\nONLY BGR24 AND BGRA32 ARE SUPPORTED" << std::endl;
+		throw std::runtime_error(filePath.string() + ": Color encoding unsupported");
+	}
 }
 
 // replace return type by BYTE* later!
@@ -51,11 +95,24 @@ unsigned char	*parseBMPFile(const std::filesystem::path &filePath, int *width, i
 	if (!doesFileExist(filePath.string()))
 	{
 		std::cout << "NO ACCESS TO " << filePath << " (INEXISTANT OR FORBIDDEN)" << std::endl;
-		throw std::exception();
+		throw std::runtime_error(filePath.string() + " inexistant or forbidden");
 	}
 	std::ifstream	bmp = openIFile(filePath);
 
-	// parse File Header
+	t_FileHeader	fileHeader;
+	readNBytes(bmp, reinterpret_cast<char*>(&fileHeader), sizeof(fileHeader));
+	printFileHeader(fileHeader); // debug
+	if (fileHeader.fileType != 0x4D42)
+	{
+		std::cout << filePath << " IS NOT A BMP FILE" << std::endl;
+		throw std::runtime_error(filePath.string() + " is not a bmp file");
+	}
+
+	t_InfoHeader	infoHeader;
+	readNBytes(bmp, reinterpret_cast<char*>(&infoHeader), sizeof(infoHeader));
+	printInfoHeader(infoHeader); // debug
+	checkInfoHeader(filePath, infoHeader);
+
 	// parse Info Header
 	// parse Color Header
 	// parse pixel data into BYTE* and return it
